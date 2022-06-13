cb81ce0faa840bbf8e10018ce15b7885cbb88902
Edwin Echeverria <edwindanielnow@gmail.com>
2022-06-13 14:40:28 -0400

    changes

--- script.js/Filter-btn.js
+++ script.js/Filter-btn.js
@@ -32,3 +32,61 @@ function loginBtn() {
     const x = document.getElementById("user-login");
     x.classList.toggle("hidden-five");
 }
+
+function carBtn() {
+    const x = document.getElementById("car-list");
+    x.classList.toggle("hidden-eight");
+}
+// Samsung
+
+// function optionOne() {
+//     const x = document.getElementById("one")
+//     console.log(x.value)
+//     x.classList.toggle("hidden-six");
+// }
+
+// function optionTwo() {
+//     const x = document.getElementById("two");
+//     x.classList.toggle("hidden-seven");
+// }
+
+// function generateHtml() {
+//     let html = '';
+//     if(x){
+//         html += `<p class="bottom-mar sml-p">
+//                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste modi id vero accusantium provident obcaecati blanditiis earum minus. Atque amet enim aut, non nisi corrupti? Magni itaque aliquam voluptatum repudiandae, cum odio placeat sint, eaque laudantium dolore pariatur, tenetur cumque ab atque provident voluptas. Incidunt similique accusamus pariatur hic dolor temporibus, eius, facilis fuga consectetur molestias possimus quis nulla consequuntur culpa numquam doloremque natus exercitationem aliquid expedita ratione sit, velit iusto laudantium. Omnis, obcaecati debitis dignissimos corporis velit autem nemo alias, harum consequatur voluptatem facere aliquid? Sapiente nihil eos minima soluta, necessitatibus ea, neque incidunt laudantium consequatur officiis sed magni?
+//                 </p>`;
+//     } else {
+//         html += `<div class="as p-size id="two">
+//         <div class="top-mar">
+//             <div class="d1-d">
+//                 <p class="bottom-mar sml-p">Tipo de pantalla</p>
+//                 <p class="bottom-mar sml-p">Tamaño de pantalla</p>
+//                 <p class="bottom-mar sml-p">RAM</p>
+//                 <p class="bottom-mar sml-p">Cámara principal</p>
+//                 <p class="bottom-mar sml-p">Cámara frontal</p>
+//                 <p class="bottom-mar sml-p">Sistema operativo</p>
+//                 <p class="bottom-mar sml-p">Puerto de carga</p>
+//                 <p class="bottom-mar sml-p">Memoria expandible</p>
+//                 <p class="bottom-mar sml-p">NFC</p>
+//             </div>
+//         </div>
+//         <div class="top-mar">
+//             <div class="d1-e">
+//                 <p class="bottom-mar2 txt-c sml-p">Super amoled</p>
+//                 <p class="bottom-mar2 txt-c sml-p">6.4"</p>
+//                 <p class="bottom-mar2 txt-c sml-p">4GB</p>
+//                 <p class="bottom-mar2 txt-c sml-p">64 + 8 + 5 + 5 mp</p>
+//                 <p class="bottom-mar2 txt-c sml-p">20 mp</p>
+//                 <p class="bottom-mar2 txt-c sml-p">Android 11 One UI 3</p>
+//                 <p class="bottom-mar2 txt-c sml-p">USB Tipo C</p>
+//                 <p class="bottom-mar2 txt-c sml-p">Hasta 512 GB</p>
+//                 <p class="bottom-mar2 txt-c sml-p">No</p>
+//             </div>
+//         </div>
+//     </div>`;
+//     }
+//     const container = document.getElementById('one');
+//     container.innerHTML = html;
+// }
+
